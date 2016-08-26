a424dd179c7a3d6093f46fe9f2ffc30e3fe8478a
ceasar <cesar.bolanos.andino@gmail.com>
2016-08-26 11:44:41 -0500

    Annotating property model

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: properties
+#
+#  id               :integer          not null, primary key
+#  title            :string           not null
+#  property_type_id :integer
+#  description      :text             not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class Property < ApplicationRecord
   belongs_to :property_type
 end
