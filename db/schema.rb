3167b3013c78957d69b3d4d59d312ffe4c9f025c
ceasar <cesar.bolanos.andino@gmail.com>
2016-08-26 11:58:38 -0500

    Create join table between features and properties

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826164305) do
+ActiveRecord::Schema.define(version: 20160826165131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20160826164305) do
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "features_properties", id: false, force: :cascade do |t|
+    t.integer  "feature_id"
+    t.integer  "property_id"
+    t.integer  "quantity",    default: 0, null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.index ["feature_id"], name: "index_features_properties_on_feature_id", using: :btree
+    t.index ["property_id"], name: "index_features_properties_on_property_id", using: :btree
   end
 
   create_table "properties", force: :cascade do |t|
