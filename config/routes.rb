4afc0e0eb60df7d2a5ea9303ad1b0ec724170e7c
ceasar <cesar.bolanos.andino@gmail.com>
2016-08-26 16:56:08 -0500

    Exposing routes for different resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'properties#index'
+
+  # Resources for properties
+  resources :properties
+
+  # Resources for features
+  resources :features
+
+  # Resources for photos
+  resources :photos
 end
