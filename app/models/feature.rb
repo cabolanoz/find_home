5234fc28756bbb3f4f30960c57f42f6ccd5cf366
ceasar <cesar.bolanos.andino@gmail.com>
2016-08-26 11:40:38 -0500

    Annotating features model

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: features
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Feature < ApplicationRecord
 end
