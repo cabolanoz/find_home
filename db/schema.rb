5a0b9c7c540978f392b37a84666e21c28c8b90fc
ceasar <cesar.bolanos.andino@gmail.com>
2016-08-26 12:11:13 -0500

    Updating schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826165131) do
+ActiveRecord::Schema.define(version: 20160826171030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 20160826165131) do
     t.index ["property_id"], name: "index_features_properties_on_property_id", using: :btree
   end
 
+  create_table "photos", force: :cascade do |t|
+    t.string   "uuid",        null: false
+    t.integer  "property_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["property_id"], name: "index_photos_on_property_id", using: :btree
+  end
+
   create_table "properties", force: :cascade do |t|
     t.string   "title",            null: false
     t.integer  "property_type_id"
@@ -47,5 +55,6 @@ ActiveRecord::Schema.define(version: 20160826165131) do
     t.datetime "updated_at",                null: false
   end
 
+  add_foreign_key "photos", "properties"
   add_foreign_key "properties", "property_types"
 end
