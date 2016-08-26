f2be5f5bfac8a73adf998a44238ed39222d0cedb
ceasar <cesar.bolanos.andino@gmail.com>
2016-08-26 11:43:56 -0500

    Disabling null fields on properties table

--- db/migrate/20160826164305_create_properties.rb
+++ db/migrate/20160826164305_create_properties.rb
@@ -1,9 +1,9 @@
 class CreateProperties < ActiveRecord::Migration[5.0]
   def change
     create_table :properties do |t|
-      t.string :title
+      t.string :title, null: false
       t.references :property_type, foreign_key: true
-      t.text :description
+      t.text :description, null: false
 
       t.timestamps
     end
