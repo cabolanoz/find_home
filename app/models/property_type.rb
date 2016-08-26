c7da86ba3eb5d221732c2469edf1fbbf598f41da
ceasar <cesar.bolanos.andino@gmail.com>
2016-08-26 11:21:34 -0500

    Annotating property type model

--- app/models/property_type.rb
+++ app/models/property_type.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: property_types
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  status     :boolean          default(TRUE), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class PropertyType < ApplicationRecord
 end
--- test/models/property_type_test.rb
+++ test/models/property_type_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: property_types
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  status     :boolean          default(TRUE), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class PropertyTypeTest < ActiveSupport::TestCase
