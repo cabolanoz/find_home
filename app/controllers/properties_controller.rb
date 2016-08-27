32d372059b6c0acc822111077a6bd13b73ca6559
ceasar <cesar.bolanos.andino@gmail.com>
2016-08-27 14:13:30 -0500

    Styling show view on properties

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -68,7 +68,7 @@ class PropertiesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_property
-      @property = Property.friendly.find(params[:id])
+      @property = Property.includes(:property_type).friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
