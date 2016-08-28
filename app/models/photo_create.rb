49918663c3cbf534d88c39201c07099dee19b2eb
ceasar <cesar.bolanos.andino@gmail.com>
2016-08-28 14:23:44 -0500

    Resizing images when saving

--- app/models/photo_create.rb
+++ app/models/photo_create.rb
@@ -19,12 +19,21 @@ class PhotoCreate
       photo = Photo.create(property: @property, content_type: photos[key].content_type)
 
       # Create image name
-      image_name = "#{photo.uuid}#{Rack::Mime::MIME_TYPES.invert[photos[key].content_type]}"
+      image_name_200 = "#{photo.uuid}200x200#{Rack::Mime::MIME_TYPES.invert[photos[key].content_type]}"
+      image_name_1200 = "#{photo.uuid}1200x1200#{Rack::Mime::MIME_TYPES.invert[photos[key].content_type]}"
+      image_data = photos[key].read
 
+      image = Magick::Image::from_blob(image_data).first
+
+      # Resize image to 200x200
+      image = image.resize(200,200)
       # Place photo to uploads folder
-      File.open(Rails.root.join('public', 'uploads', image_name), 'wb') do |file|
-        file.write(photos[key].read)
-      end
+      image.write(Rails.root.join('public', 'uploads', "#{image_name_200}"))
+
+      # Resize image to 1200x1200
+      image = image.resize(1200,1200)
+      # Place photo to uploads folder
+      image.write(Rails.root.join('public', 'uploads', "#{image_name_1200}"))
     end
   end
 end
